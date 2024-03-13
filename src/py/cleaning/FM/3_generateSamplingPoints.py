9c1545503eda8bdb7e022ca09393cfcd9d5cafcb
math-eaton <mheaton@ciesin.columbia.edu>
2024-03-12 22:55:35 -0400

    add sampling level property to pts

--- src/py/cleaning/FM/3_generateSamplingPoints.py
+++ src/py/cleaning/FM/3_generateSamplingPoints.py
@@ -1,11 +1,10 @@
 import geojson
 from shapely.geometry import Point, LineString, mapping
 import rasterio
-from rasterio.transform import from_origin
 
 def sample_points_on_line(line, num_points):
     """Generate evenly spaced points along a line."""
-    distances = [i / num_points * line.length for i in range(1, num_points)]
+    distances = [i / num_points * line.length for i in range(1, num_points + 1)]
     return [line.interpolate(distance) for distance in distances]
 
 def add_elevation(point, raster):
@@ -33,25 +32,43 @@ def generate_spokes_with_sampling(input_geojson, output_geojson, dem_path=None):
     for feature in data['features']:
         transmitter_location = feature['properties']['transmitter_site'].split(', ')
         transmitter_point = Point(float(transmitter_location[0]), float(transmitter_location[1]))
+        lms_application_id = feature['properties']['lms_application_id']
 
         for vertex in feature['geometry']['coordinates'][0]:  # Assuming only one polygon per feature
             vertex_point = Point(vertex[0], vertex[1])
             line = LineString([transmitter_point, vertex_point])
 
-            # Sample points along the line
-            sampled_points = sample_points_on_line(line, 10)  # Adjust the number of points as needed
+            # Add the vertex point (end of the spoke) with sampling_level 0
+            output['features'].append({
+                "type": "Feature",
+                "geometry": mapping(vertex_point),
+                "properties": {
+                    "transmitter_site": feature['properties']['transmitter_site'],
+                    "lms_application_id": lms_application_id,
+                    "sampling_level": 0
+                },
+            })
 
-            for point in sampled_points:
+            # Sample points along the line
+            sampled_points = sample_points_on_line(line, 4)  # Adjust to 9 because the vertex is already included as 0
+
+            for i, point in enumerate(sampled_points, start=1):  # Start at 1 because 0 is the vertex point
                 elevation = add_elevation(point, raster) if raster else None
+                # Add point geometries with elevation and properties to the output
                 output['features'].append({
                     "type": "Feature",
                     "geometry": mapping(point),
-                    "properties": {"elevation": elevation},
+                    "properties": {
+                        "elevation": elevation,
+                        "transmitter_site": feature['properties']['transmitter_site'],
+                        "lms_application_id": lms_application_id,
+                        "sampling_level": i  # Reflects the position along the spoke's progress
+                    },
                 })
 
     # Write the output GeoJSON to a file
     with open(output_geojson, 'w') as file:
-        geojson.dump(output, file, indent=2)
+        geojson.dump(output, file, indent=4)
 
 # Example usage
 input_geojson = 'src/assets/data/fcc/fm/processed/FM_contours_AOI_polygon.geojson'  # Change this to the path of your input file
