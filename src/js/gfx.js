5638ea82e992e4c5a5046611733ab2487b0d3778
math-eaton <mheaton@ciesin.columbia.edu>
2024-03-25 12:36:39 -0400

    update data paths

--- src/js/gfx.js
+++ src/js/gfx.js
@@ -1194,17 +1194,19 @@ async function addFMTowerPts(geojson, channelFilter) {
     const matchingColor = new THREE.Color(colorScheme.matchingPyramidColor);
     const nonMatchingColor = new THREE.Color(colorScheme.nonMatchingPyramidColor);
     const matchingOpacity = 0.8;
-    const nonMatchingOpacity = 0.4;
+    const nonMatchingOpacity = 0.6;
 
     // Prepare instanced mesh materials
     const materialMatching = new THREE.MeshBasicMaterial({ 
       color: matchingColor, 
       transparent: true, 
+      alphaHash: true,
       opacity: matchingOpacity 
     });
     const materialNonMatching = new THREE.MeshBasicMaterial({ 
       color: nonMatchingColor, 
       transparent: true, 
+      alphaHash: true,
       opacity: nonMatchingOpacity 
     });
 
@@ -1740,14 +1742,14 @@ function updateVisualizationWithChannelFilter(contourGeojsonData, towerGeojsonDa
   async function loadGeoJSONData(onCriticalDataLoaded) {
     // console.log("loading...")
     const urls = [
-      'src/assets/data/public/stanford_contours_simplified1000m_20231124_simplified.geojson',
-      'src/assets/data/public/CellularTowers_FeaturesToJSON_HIFLD_AOI_20231204.geojson',
-      // 'src/assets/data/public/FmTowers_FeaturesToJSON_AOI_20231204.geojson',
-      'src/assets/data/public/study_area_admin0clip.geojson',
-      'src/assets/data/public/cellServiceCentroids_2000m_20231210.geojson',
-      'src/assets/data/public/fm_freq_dict.json',
-      'src/assets/data/public/FM_transmitter_sites.geojson',
-      'src/assets/data/internal/fcc/fm/processed/FM_service_contour_downsample12_5step_processed_FMinfoJoin_polygon_20240324.geojson'
+      'src/assets/data/stanford_contours_simplified1000m_20231124_simplified.geojson',
+      'src/assets/data/CellularTowers_FeaturesToJSON_HIFLD_AOI_20231204.geojson',
+      // 'src/assets/data/FmTowers_FeaturesToJSON_AOI_20231204.geojson',
+      'src/assets/data/study_area_admin0clip.geojson',
+      'src/assets/data/cellServiceCentroids_2000m_20231210.geojson',
+      'src/assets/data/fm_freq_dict.json',
+      'src/assets/data/FM_transmitter_sites.geojson',
+      'src/assets/data/fcc/fm/processed/FM_service_contour_downsample12_5step_processed_FMinfoJoin_polygon_20240324.geojson'
     ];
 
     let criticalDatasetsLoaded = 0;
@@ -1788,50 +1790,50 @@ function updateVisualizationWithChannelFilter(contourGeojsonData, towerGeojsonDa
 
   function handleGeoJSONData(url, data) {
     switch (url) {
-      case 'src/assets/data/public/stanford_contours_simplified1000m_20231124_simplified.geojson':
+      case 'src/assets/data/stanford_contours_simplified1000m_20231124_simplified.geojson':
         contourGeojsonData = data;
         const meanElevation = calculateMeanContourElevation(data);
         addElevContourLines(data);
         break;
 
-      case 'src/assets/data/public/CellularTowers_FeaturesToJSON_HIFLD_AOI_20231204.geojson':
+      case 'src/assets/data/CellularTowers_FeaturesToJSON_HIFLD_AOI_20231204.geojson':
         cellTowerGeojsonData = data;
         addCellTowerPts(data);
         break;
 
-      case 'src/assets/data/internal/fcc/fm/processed/FM_service_contour_downsample12_5step_processed_FMinfoJoin_polygon_20240324.geojson':
+      case 'src/assets/data/fcc/fm/processed/FM_service_contour_downsample12_5step_processed_FMinfoJoin_polygon_20240324.geojson':
         fmContoursGeojsonData = data;
         addFMpropagation3D(data);
         break;
 
 
-      // case 'src/assets/data/public/FmTowers_FeaturesToJSON_AOI_20231204.geojson':
+      // case 'src/assets/data/FmTowers_FeaturesToJSON_AOI_20231204.geojson':
       //   fmTransmitterGeojsonData = data;
       //   addFMTowerPts(data);
       //   break;
 
       // updated points using fm contour origins
-      case 'src/assets/data/public/FM_transmitter_sites.geojson':
+      case 'src/assets/data/FM_transmitter_sites.geojson':
         fmTransmitterGeojsonData = data;
         addFMTowerPts(data);
         break;
 
-      case 'src/assets/data/public/study_area_admin0clip.geojson':
+      case 'src/assets/data/study_area_admin0clip.geojson':
         boundingBoxGeojsonData = data;
         // visualizeBoundingBoxGeoJSON(data);
         break;
 
-      case 'src/assets/data/public/NYS_cellTower_viewshed_20231130.jpg':
+      case 'src/assets/data/NYS_cellTower_viewshed_20231130.jpg':
         viewshedJPG = data;
         // loadAndPositionRaster(data);
         break;
 
-      case 'src/assets/data/public/cellServiceCentroids_2000m_20231210.geojson':
+      case 'src/assets/data/cellServiceCentroids_2000m_20231210.geojson':
         cellServiceGeojsonData = data;
         // addCellServiceMesh(data);
         break;
 
-      case 'src/assets/data/public/fm_freq_dict.json':
+      case 'src/assets/data/fm_freq_dict.json':
         fmFreqDictionaryJson = data;
         break;
   
